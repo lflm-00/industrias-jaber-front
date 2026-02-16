eaa6f5c23103fe4405925a92a9b9a1d028e69e17
Luis Fernando L <luis.lopezm@pragma.com.co>
2026-02-16 11:10:40 -0500

    change path to acces contact

--- components/Machinery/MachineryCTA.tsx
+++ components/Machinery/MachineryCTA.tsx
@@ -18,7 +18,7 @@ export default function MachineryCTA() {
           </div>
           <div className="flex flex-wrap justify-center gap-4">
             <Link
-              href="/Contact">
+              href="/contact">
               <button className="flex min-w-[200px] cursor-pointer items-center justify-center rounded-xl h-14 px-8 bg-buttons text-white text-base font-bold shadow-xl shadow-primary/30 transition-all hover:scale-105">
                 <span className="material-symbols-outlined mr-2">mail</span>
                 Request Consultation
